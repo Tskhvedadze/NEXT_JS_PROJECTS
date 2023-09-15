188b38569c59c2fdaf51a301b8aaffbe04f71e75
Tskhvedadze <giorgicxvedadze4@gmail.com>
2023-09-15 11:41:06 +0400

    completed next01

---
+++ next01/app/about/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function About() {
+  //!   throw new Error("Error");
+
+  return (
+    <>
+      <h1>About</h1>
+      <Link href={"/"}>Link to Home Page</Link>
+    </>
+  );
+}
