a7f22a93d108e8d1e8456b71cdfdc6fd75a73a50
Tskhvedadze <giorgicxvedadze4@gmail.com>
2023-09-18 14:51:37 +0400

    completed next06

---
+++ next06/app/components/index.ts
@@ -0,0 +1,3 @@
+export { Navbar } from "./Navbar";
+export { MyProfilePic } from "./MyProfilePic";
+export { Posts } from "./Posts";
---
+++ next06/app/layout.tsx
@@ -0,0 +1,23 @@
+import { Navbar, MyProfilePic } from "./components";
+import "./globals.css";
+
+export const metadata = {
+  title: "Dave's Blog",
+  description: "Created by Dave Gray",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="dark:bg-slate-800">
+        <Navbar />
+        <MyProfilePic />
+        {children}
+      </body>
+    </html>
+  );
+}
