3efbce76120667129678363ab651e24c58473393
Tskhvedadze <giorgicxvedadze4@gmail.com>
2023-09-18 17:34:18 +0400

    middleware

--- next08/src/app/api/hello/route.ts
+++ next08/src/app/api/hello/route.ts
@@ -1,3 +1,23 @@
+import { NextResponse } from "next/server";
+import { limiter } from "../config/limiter";
+
 export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+  const remaining = await limiter.removeTokens(1);
+
+  const origin = request.headers.get("origin");
+
+  console.log("remaining: ", remaining);
+
+  if (remaining < 0) {
+    return new NextResponse(null, {
+      status: 429,
+      statusText: "Too May Requests",
+      headers: {
+        "Access-Control-Allow-Origin": origin || "*",
+        "Content-Type": "Text/plaim",
+      },
+    });
+  } else {
+    return new Response("Hello, Next.js!");
+  }
 }
--- next08/src/app/api/todos/[id]/route.ts
+++ next08/src/app/api/todos/[id]/route.ts
@@ -2,9 +2,15 @@ import { NextResponse } from "next/server";
 
 const DATA_SRC_URL = "https://jsonplaceholder.typicode.com/todos";
 
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
 //* **************************** GET REQUEST (Single todo) ****************************
-export async function GET(request: Request) {
-  const id = request.url.slice(request.url.lastIndexOf("/") + 1);
+export async function GET(request: Request, { params: { id } }: Props) {
+  // const id = request.url.slice(request.url.lastIndexOf("/") + 1);
 
   const res = await fetch(`${DATA_SRC_URL}/${id}`);
 
